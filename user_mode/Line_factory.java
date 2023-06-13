dc16e51f81011fb2ad45ea0af9b480bc49f75642
Ivy Chang <ivy900403@gmail.com>
2023-06-13 18:38:57 +0800

    change to Object factory

--- user_mode/Line_factory.java
+++ user_mode/Line_factory.java
@@ -1,11 +1,15 @@
 package user_mode;
 
+import java.awt.Point;
 import base_classes.Association_line;
+import base_classes.Basic_object;
+import base_classes.Class_object;
 import base_classes.Composition_line;
 import base_classes.Generalization_line;
 import base_classes.Line;
+import base_classes.Use_case;
 
-public class Line_factory {
+public class Line_factory implements Object_factory_interface{
     private String lineType;
     public Line_factory(String lineType){
         this.lineType = lineType;
@@ -19,6 +23,15 @@ public class Line_factory {
         }
         else if(lineType.equals("compo")){
             return new Composition_line(start_x, start_y, end_x, end_y);
+        }
+        return null;
+    }
+    public Basic_object createBasicObject(Point p, int start_x, int start_y){
+        if(lineType.equals("useCase")){
+            return new Use_case(p, start_x, start_y);
+        }
+        else if(lineType.equals("classObj")){
+            return new Class_object(p, start_x, start_y);
         }
         return null;
     }
---
+++ user_mode/Object_factory_interface.java
@@ -0,0 +1,10 @@
+package user_mode;
+
+import java.awt.Point;
+import base_classes.Basic_object;
+import base_classes.Line;
+
+public interface Object_factory_interface {
+    public Basic_object createBasicObject(Point p, int start_x, int start_y);
+    public Line createLine(int start_x, int start_y, int end_x, int end_y);
+}
